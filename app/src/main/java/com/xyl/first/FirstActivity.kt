b927b33b01c29a7fd00f0cd9979beb7f64a4c665
xieyulei <15812638782@163.com>
2022-06-10 17:46:02 +0800

    4.Android基础知识点(Activity)

---
+++ app/src/main/java/com/xyl/first/FirstActivity.kt
@@ -0,0 +1,18 @@
+package com.xyl.first
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Copyright (c) 2022 Raysharp.cn. All rights reserved.
+ *
+ * FirstActivity
+ * @author xieyulei
+ * @date 2022-06-10
+ */
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+}
--- app/src/main/java/com/xyl/first/MainActivity.kt
+++ app/src/main/java/com/xyl/first/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.xyl.first
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnTest = findViewById<Button>(R.id.btn_test)
+        btnTest.setOnClickListener {
+            startActivity(Intent(this, FirstActivity::class.java))
+        }
     }
 }
